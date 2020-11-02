62d5476741cc83b623dcbfd75c83cfb55432e7e4
Josh Cornau <hi@joshcornau.de>
2020-11-02 20:42:09 +0100

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="uk-container uk-container-xlarge">
+    <nav class="uk-navbar-container uk-text-bold" uk-navbar>
+      <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+          <li>
+            <a class="uk-text-primary" href="/">Josh Cornau</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="uk-navbar-right">
+        <ul class="uk-navbar-nav" >
+          <li   v-for="category in categories" v-bind:key="category.id">
+            <router-link class="uk-text-primary"
+              :to="{ path: '/category/' + category.id }"
+              :key="category.id"
+            >
+              {{ category.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  name: "Nav",
+  data() {
+    return {
+      categories: []
+    };
+  },
+  apollo: {
+    categories: gql`
+      query Categories {
+        categories {
+          id
+          name
+        }
+      }
+    `
+  }
+};
+</script>
+
+<style lang="css">
+.uk-navbar-nav{
+  font-family: Staatliches;
+}
+
+</style>
---
+++ src/containers/template.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    VueMarkdownIt
+  },
+  apollo: {
+    article: {
+      query: gql`
+        query Articles($id: ID!) {
+          article(id: $id) {
+            id
+            title
+            content
+            image {
+              url
+            }
+            published_at
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.routeParam
+        };
+      }
+    }
+  }
+};
+</script>
