edc45386d9591758393004a26277c3be50e1bff3
Josh Cornau <hi@joshcornau.de>
2020-11-07 14:40:51 +0100

    added TestSide and font-style css

---
+++ src/containers/TestSide.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Heading h1</h1>
+        <h2>Heading h2</h2>
+        <h3>Heading h1</h3>
+        <p class="test-paragraph paragraph">
+            There are tons of screens and devices with different heights and widths, so it is hard to create an exact breakpoint for each device. To keep things simple you could target five common groups:
+        </p>
+
+
+<h1><p>Hi, I'm Josh! Welcome to my portfolio. I am a <span style="color:#374cff;"> UX/UI designer</span>  based in Stuttgart, Germany. Currently I'm getting ready for my master studies. 
+Meanwhile I'm working as a freelancer in my field.</p><p>I would describe myself as a <span style="color:#374cff;">professional generalist.</span> But see for yourself.</p></h1>
+    </div>
+</template>
