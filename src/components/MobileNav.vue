656b7f0bf0160eb0c3005aa2b4897a43a1957e61
Josh Cornau <hi@joshcornau.de>
2020-11-08 16:04:23 +0100

    Added new File MobileNav

---
+++ src/components/MobileNav.vue
@@ -0,0 +1,50 @@
+/* eslint-disable */
+<template>
+  <nav class="sticky">
+    <ul class="menu-mobile">
+      <li class="logo">
+        <router-link :to="{ path: '/' }">
+          Josh Cornau
+        </router-link>
+      </li>
+      <li class="item">
+        <router-link :to="{ path: '/' }">
+          Work
+        </router-link>
+      </li>
+      <li class="item" v-for="category in categories" v-bind:key="category.id">
+        <router-link
+          :to="{ path: '/category/' + category.id }"
+          :key="category.id"
+        >
+          {{ category.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  name: "Nav",
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  apollo: {
+    categories: gql`
+      query Categories {
+        categories {
+          id
+          name
+        }
+      }
+    `,
+  },
+};
+</script>
+
+<style lang="css"></style>
