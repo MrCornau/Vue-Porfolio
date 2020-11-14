9f2483514cbadb6f9b9294274ec5e8f9ec4e1127
Josh Cornau <hi@joshcornau.de>
2020-11-14 15:04:53 +0100

    created Tag Component

--- src/components/ArticlesList.vue
+++ src/components/ArticlesList.vue
@@ -1,54 +1,50 @@
 <template>
+  <div>
     <div>
-      <div>
       <div class="heading">
-        <h1>Selected Projects </h1>
+        <h1>Selected Projects</h1>
       </div>
-        <div class="articles">
-          <router-link
-            v-for="(article, index) in articles"
-            :to="{ path: '/article/' + article.id }"
-            class=""
-            :key="article.id"
-          >
-            <div class="articles-main">
-              <div class="info " v-bind:class="{ 'order-info' : index%2 == 1, }">
-                <p>{{ article.Year}}</p>
-                <p>test{{ index}}</p>
-                <h2> {{ article.title }}</h2>
-                <p> {{article.Description}}</p>
-                <div class="tag-container red">
-                  <div class="tag" v-for="tag in article.Tags.tags" :key="tag.id">
-                    <i class="fas fa-clock" style="font-size:24px;"></i>
-                    <p>{{tag.content}}</p>
-                  </div>
-              </div>
-              
-              </div>
-                <div class="distance-articles" v-bind:class="{ 'order-margin' : index%2 == 1 }"> </div>
-              <div class="image-box">
-                <img
+      <div class="articles">
+        <router-link
+          v-for="(article, index) in articles"
+          :to="{ path: '/article/' + article.id }"
+          class=""
+          :key="article.id"
+        >
+          <div class="articles-main">
+            <div class="info " v-bind:class="{ 'order-info': index % 2 == 1 }">
+              <h4 class="articles-preview-margins">{{ article.Year }}</h4>
+              <h2 class="articles-preview-margins">{{ article.title }}</h2>
+              <p class="articles-preview-marginl">{{ article.Description }}</p>
+              <Tags v-bind:tags="article.Tags.tags" />
+            </div>
+            <div
+              class="distance-articles"
+              v-bind:class="{ 'order-margin': index % 2 == 1 }"
+            ></div>
+            <div class="image-box">
+              <img
                 class="Article-Image"
-                  :src="api_url + article.image[0].url"
-                  alt="nothing"
-                />
-              </div>
+                :src="api_url + article.image[0].url"
+                alt="nothing"
+              />
+            </div>
 
-<!--               
+            <!--               
               <div class="tags red">
                 <p id="category" v-if="article.category" class="">
                   {{ article.category.name }}
                 </p>
               </div> -->
-            </div>
-          </router-link>
-        </div>
+          </div>
+        </router-link>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script>
+import Tags from "../components/TagPreview.vue";
 
 export default {
   data: function() {
@@ -56,18 +52,17 @@ export default {
       api_url: process.env.VUE_APP_STRAPI_API_URL,
       header: [],
     };
-  }
-  ,
+  },
   props: {
-    articles: Array
+    articles: Array,
   },
-  computed: {
+  components: {
+    Tags,
   },
-  created: function () {
+  computed: {},
+  created: function() {
     // `this` points to the vm instance
-    console.log('a is: ' + this.articles)
-  }
+    console.log("a is: " + this.articles);
+  },
 };
 </script>
-
-
---
+++ src/components/TagPreview.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="tag-container">
+                  <div class="tag" v-for="tag in tags" :key="tag.id">
+                    <i class="fas fa-clock" style="font-size:24px;"></i>
+                    <p>{{tag.content}}</p>
+                  </div>
+</div>
+</template>
+<script>
+export default {
+    props:{
+        tags: Array
+    }
+}
+</script>
