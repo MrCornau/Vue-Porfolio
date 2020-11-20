2762fc7811e0b4df928fcdd62786c182f71ad395
Josh Cornau <hi@joshcornau.de>
2020-11-20 08:38:09 +0100

    added article.css & article-detail.vue

--- src/containers/Article.vue
+++ src/containers/Article.vue
@@ -1,39 +1,42 @@
 <template>
   <div>
-    <div
-      v-if="article.image"
-      class=""
-      :data-src="api_url + article.image[0].url"
-    >
-      <h1>{{ article.title }}</h1>
-      <h1>{{ article.title }}</h1>
-      <h1>{{ article.title }}</h1>
-      <h1>{{ article.title }}</h1>
-      <h1>{{ article.title }}</h1>
+    <div class="article-detail__header-image">
+      <img
+        v-if="article.Header_Image"
+        class=""
+        :src="api_url + article.Header_Image.url"
+        alt="nothing"
+      />
     </div>
 
-    <div class="uk-section">
-      <div class="uk-container uk-container-small">
-        <vue-markdown-it
-          v-if="article.content"
-          :source="article.content"
-          id="editor"
-        />
-        <p v-if="article.published_at">
-          {{ moment(article.published_at).format("MMM Do YY") }}
-        </p>
+    <div class="article-detail__container article-detail__width">
+      <div class="article-detail__heading">
+        <h1>{{ article.title }}</h1>
+        <h1>{{ article.Header_Explanation }}</h1>
       </div>
+
+    <div v-if="article.content" class="article-detail__info">
+      <h3>{{article.content}}</h3>
+    </div>
+    <div class="article-detail__domains">
+      <h2>Domains</h2>
+      <div class="article-detail_domains__Tags">
+        <div v-for="(tag,index) in article.Tags.tags" v-bind:key="tag.id"  class="article-detail_domains__Tags--margin">
+            <div v-if="index<4" class="article-detail_domains__Tags__Tag" v-bind:style="{ 'background-color': article.color}"> 
+              <box-icon v-bind:name="tag.icon" size="lg" animation='tada-hover' color="#253031"></box-icon>
+               <p>{{ tag.content }}</p>
+            </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   </div>
 </template>
 
 <script>
 var moment = require("moment");
-import VueMarkdownIt from "vue-markdown-it";
 import gql from "graphql-tag";
-
-
-
 
 export default {
   data() {
@@ -41,59 +44,64 @@ export default {
       article: {},
       moment: moment,
       api_url: process.env.VUE_APP_STRAPI_API_URL,
-      routeParam: this.$route.params.id
+      routeParam: this.$route.params.id,
     };
   },
   components: {
-    VueMarkdownIt
   },
   apollo: {
-
     article: {
       query: gql`
         query Articles($id: ID!) {
-           article(id: $id) {
+          article(id: $id) {
             id
-              title
-            	Year
-              content
-              Tags
-              Description
-              image {
-                url
-              }
-              Detail{
-                ... on ComponentProjectsGraphic{
-                  id
-                  Solution_Text
-                  Solution_graphic {url}
-                  
-                }
-                ... on ComponentProjectsHmw{
-                  id
-                  HowMightWe
-                }
-                ... on ComponentProjectsOutline{
-                  id
-                  project_info
-                  project_role
-                }
-                 ... on ComponentProjectsTheProblem{
-                  id
-                  Problem_text
-                  Problem_illustration{url}
+            title
+            Year
+            content
+            Tags
+            color
+            Description
+            Header_Image {
+              url
+            }
+            Header_Explanation
+            image {
+              url
+            }
+            Detail {
+              ... on ComponentProjectsGraphic {
+                id
+                Solution_Text
+                Solution_graphic {
+                  url
                 }
               }
-              
+              ... on ComponentProjectsHmw {
+                id
+                HowMightWe
+              }
+              ... on ComponentProjectsOutline {
+                id
+                project_info
+                project_role
+              }
+              ... on ComponentProjectsTheProblem {
+                id
+                Problem_text
+                Problem_illustration {
+                  url
+                }
+              }
+            }
           }
         }
       `,
       variables() {
         return {
-          id: this.routeParam
+          id: this.routeParam,
         };
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
