d8e8cd0d3075d4015e54bf976a9b7599f857193f
Josh Cornau <hi@joshcornau.de>
2021-02-25 16:00:55 +0100

    createdHeadVue

--- src/containers/about.vue
+++ src/containers/about.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="About">
 
-      <h1>Hallo Welt</h1>
-       <h1>Hallo Welt</h1>
-        <h1>Hallo Welt</h1>
-         <h1>Hallo Welt</h1>
-          <h1>Hallo Welt</h1>
   </div>
 </template>
 
